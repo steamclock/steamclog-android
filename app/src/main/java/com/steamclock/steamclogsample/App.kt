22c12bfac9c1cd82ecf5160598f92ab2d56e0f61
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2020-07-16 12:49:40 -0700

    Reset project to fix up bad package names

---
+++ app/src/main/java/com/steamclock/steamclogsample/App.kt
@@ -0,0 +1,15 @@
+package com.steamclock.steamclogsample
+
+import android.app.Application
+import com.steamclock.steamclog.clog
+
+/**
+ * steamclog
+ * Created by jake on 2020-03-27, 2:40 PM
+ */
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        clog.initialize(this)
+    }
+}
